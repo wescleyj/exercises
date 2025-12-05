b42095838c8077a7226f96ee01eab71cdd3078be
wescleyj <wescleyjnavarro@gmail.com>
2025-12-05 16:57:33 -0300

    Configurar entity ninja

---
+++ JavaSpringBoot/CadastroDeNinjas/src/main/java/dev/java10x/cadastrodeninjas/NinjaModel.java
@@ -0,0 +1,55 @@
+package dev.java10x.cadastrodeninjas;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
