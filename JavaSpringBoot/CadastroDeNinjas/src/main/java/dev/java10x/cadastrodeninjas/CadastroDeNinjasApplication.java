9280474d0bd9f7c200d79c4c53bfde50750ed8f4
wescleyj <wescleyjnavarro@gmail.com>
2025-12-05 15:57:17 -0300

    Iniciando aprendizado SpringBoot

---
+++ JavaSpringBoot/CadastroDeNinjas/src/main/java/dev/java10x/cadastrodeninjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastrodeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+    void main(String[] args) {
+        SpringApplication.run(CadastroDeNinjasApplication.class, args);
+    }
+
+}
---
+++ JavaSpringBoot/CadastroDeNinjas/src/test/java/dev/java10x/cadastrodeninjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastrodeninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
